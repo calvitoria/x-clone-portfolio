21b09c48755c1af8fbcebd2d0aa4629841565484
Vitória Calvi <calvi.vitoria@gmail.com>
2024-10-23 22:13:26 -0300

    add banner component and improve some component tests

--- test/components/banner_component_test.rb
+++ test/components/banner_component_test.rb
@@ -3,10 +3,19 @@
 require "test_helper"
 
 class BannerComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(BannerComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_component_renders_correctly
+    # Render the component
+    rendered_component = render_inline(BannerComponent.new)
+
+    assert_includes rendered_component.to_html, "https://github.com/user-attachments/assets/ccebd1ba-1b9e-4d61-9ffc-5caefad25c3f"
+    assert_includes rendered_component.to_html, "https://i.pinimg.com/564x/59/f3/16/59f3168b5b459eb40968234d1ea86bfb.jpg"
+    
+    assert_selector "div.avatar" do
+      assert_selector "img[src='https://github.com/user-attachments/assets/ccebd1ba-1b9e-4d61-9ffc-5caefad25c3f']"
+    end
+    
+    assert_selector "figure" do
+      assert_selector "img[src='https://i.pinimg.com/564x/59/f3/16/59f3168b5b459eb40968234d1ea86bfb.jpg']"
+    end
   end
 end
